f55c0b1c72c68243630000f05074894782f54a52
Jorge Gravel <jorge.gravel.g@gmail.com>
2024-01-22 20:39:19 +0100

    Cambios responsive layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink, Outlet } from 'react-router-dom';
 
 const App = () => {
+  const [isMenuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!isMenuOpen);
+  };
+
   return (
     <>
       <header>
+        <div className={`sidenav ${isMenuOpen ? 'open' : ''}`}>
+          <div className="menu-icon" onClick={toggleMenu}>
+            <div className="bar1"></div>
+            <div className="bar2"></div>
+            <div className="bar3"></div>
+          </div>
+          <nav>
+            <NavLink className="NavLink" to="" onClick={toggleMenu}>
+              Home
+            </NavLink>
+            <NavLink className="NavLink" to="cities-temp" onClick={toggleMenu}>
+              Cities Temp.
+            </NavLink>
+            <NavLink className="NavLink" to="local" onClick={toggleMenu}>
+              Local Forecast
+            </NavLink>
+            <NavLink className="NavLink" to="cities" onClick={toggleMenu}>
+              Cities Forecast
+            </NavLink>
+          </nav>
+        </div>
         <h1>Weather App</h1>
-        <nav>
-          <NavLink className="NavLink" to="">
-            Home{' '}
-          </NavLink>
-          <NavLink className="NavLink" to="cities-temp">
-            Cities Temp.{' '}
-          </NavLink>
-          <NavLink className="NavLink" to="local">
-            Local Forecast{' '}
-          </NavLink>
-          <NavLink className="NavLink" to="cities">
-            Cities Forecast{' '}
-          </NavLink>
-        </nav>
       </header>
       <main>
-        <div className="fristContainer">
+        <div className="firstContainer">
           <Outlet />
         </div>
       </main>
